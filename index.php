5f08184f0fb9c00555be5ea5cdf6c43d04f45d1d
Daniel Gomes <danfergo@gmail.com>
2014-11-18 19:06:47 +0000

    adding index.php something. again.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Insert title here</title>
+    </head>
+    <body>
+        Hello world!
+        <?php echo "<br>testing php <br>"?>
+    </body>
+</html>
