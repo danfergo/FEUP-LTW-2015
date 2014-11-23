98d3b35a0d448846f6df94ed4c087a96bacca3ed
Daniel Gomes <danfergo@gmail.com>
2014-11-23 12:58:44 +0000

    add user class.

--- classes/user.php
+++ classes/user.php
@@ -1,15 +1,31 @@
-
-
-
-<?php
+    <?php
 
 class User{
-    private username;
-    private email;
-    private birthday;
-    private fullName;
+    private $userid;
+    private $username;
+    private $email;
+    private $birthday;
+    private $fullName;
     
     
+    public function __construct($userid,$username,$email,$birthday,$fullName) {
+        $this->userid = $userid;
+        $this->username = $username;
+        $this->email = $email;
+        $this->birthday = $birthday;
+        $this->fullName = $fullName;
+    }
     
+    
+    public function toJSON(){
+        $data = array(
+            'userid' => $this->userid,
+            'username' => $this->username,
+            'full_name' => $this->fullName,
+            'email' => $this->email,
+            'birthay' => $this->birthday
+            );
+        return json_encode($data);
+    }
     
 }
