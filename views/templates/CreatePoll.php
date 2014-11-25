6ccf01096b854413968988c81672efea084dd4ed
Nelson <paracontas987@hotmail.com>
2014-11-25 17:26:12 +0000

    Poll creation

---
+++ views/templates/CreatePoll.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Creating a Poll</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div>
+            <form action="newpoll.php" method="get">
+                <ol>
+                    <li><label><strong>Question</strong>
+                    <input type="text" name="Question" required="required">
+                    </label><ol>
+                        <li><label><strong>Answer</strong>
+                            <input type="text" name="Answer" required="required">
+                            </label>
+                        </li>
+                        <li><label><strong>Answer</strong>
+                            <input type="text" name="Answer" required="required">
+                            </label>
+                        </li>
+                    </ol></li>
+                </ol>
+                <input type="submit" value="submit">
+            </form>
+        </div>
+    </body>
+</html>
