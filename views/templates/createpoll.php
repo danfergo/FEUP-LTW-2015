c070e2e8181cdf1140cf120783e52fe416dbe9ce
Nelson <paracontas987@hotmail.com>
2014-11-26 02:30:14 +0000

    Poll creation html implementado | falta guardar a informacao na base de dados

--- views/templates/createpoll.php
+++ views/templates/createpoll.php
@@ -2,13 +2,22 @@
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script>
         var num_question = 1;
-        function addAnswer(elem) { //terminar numero das resposta
-            $('#newAnswer p:last').before( '<p><li><label><strong>Answer </strong><input type="text" name="Answer" required="required"></label></li></p>' );
+        function addAnswer(num) {
+            $('#Answers'+num+' li:last').after( '<li><label><strong>Answer </strong><input type="text" name="answer" required="required"></label></li>' );
             }
         function addQuestion() {
-            $('#newQuestion p:last').before( '<p><li><label><strong>Question</strong><input type="text" name="Question" required="required"></label><ol id="newAnswer'+1+'"><p><li><label><strong>Answer</strong><input type="text" name="Answer" required="required"></label></li></p><p><li><label><strong>Answer</strong><input type="text" name="Answer" required="required"></label></li></p><button onclick="addAnswer(); return false;"><strong>+</stron></button></ol></li></p>' );
+            $('#Questions div:last').after( '<div><li><label><strong>Question </strong><input type="text" name="question" required="required"></label><br><label>Number of choiceable answers:<input type= "number" name="num_choiceable_answers" min="0" max="20"></label><ol id="Answers'+num_question+'"><li><label><strong>Answer </strong><input type="text" name="answer" required="required"></label></li><li><label><strong>Answer </strong><input type="text" name="answer" required="required"></label></li><button onclick="addAnswer('+num_question+'); return false;"><strong>+</stron></button><button onclick="removeAnswer('+num_question+'); return false;"><strong>-</stron></button></ol></li></div>' );
             num_question++;
             }
+        function removeAnswer(num) {
+            if($('#Answers'+num+'> li').length > 2)    $('#Answers'+num+' li:last').remove();
+        }
+        function removeQuestion() {
+            if(num_question > 1){   
+                    $('#Questions div:last').remove();
+                    num_question--;
+                }
+        }
     </script>
 
     <head>
@@ -18,24 +27,26 @@
     <body>
         <div>
             <form action="newpoll.php" method="get">
-                <ol id="newQuestion">
-                    <p><li><label><strong>Question</strong>
-                    <input type="text" name="Question" required="required">
-                        </label>
-                    <ol id="newAnswer">
-                        <p><li><label><strong>Answer</strong>
-                            <input type="text" name="Answer" required="required">
+                <ol id="Questions">
+                    <div><li><label><strong>Question </strong>
+                                <input type="text" name="question" required="required"></label><br>
+                    <label>Number of choiceable answers:<input type= "number" name="num_choiceable_answers" min="0" max="20"></label>
+                    <ol id="Answers0">
+                        <li><label><strong>Answer </strong>
+                            <input type="text" name="answer" required="required">
                             </label>
-                        </li></p>
-                        <p><li><label><strong>Answer</strong>
-                            <input type="text" name="Answer" required="required">
+                        </li>
+                        <li><label><strong>Answer </strong>
+                            <input type="text" name="answer" required="required">
                             </label>
-                        </li></p>
-                        <button onclick="addAnswer(); return false;"><strong>+</stron></button>
-                    </ol></li></p>
+                        </li>
+                        <button onclick="addAnswer(0); return false;"><strong>+</stron></button>
+                            <button onclick="removeAnswer(0); return false;"><strong>-</stron></button>
+                    </ol></li></div>
                     <button onclick="addQuestion(); return false;"><strong>+</stron></button>
+                        <button onclick="removeQuestion(); return false;"><strong>-</stron></button>
                 </ol>
-                <input type="submit" value="submit">
+                <input type="submit" value="Submit">
             </form>
         </div>
     </body>
