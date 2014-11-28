3e2b75ea2cb2784ad57dacc54cc7057a67e8897e
Nelson <paracontas987@hotmail.com>
2014-11-28 16:35:35 +0000

    Poll creation html implementado validate form

--- views/templates/createpoll.php
+++ views/templates/createpoll.php
@@ -3,21 +3,46 @@
     <script>
         var num_question = 1;
         function addAnswer(num) {
-            $('#Answers'+num+' li:last').after( '<li><label><strong>Answer </strong><input type="text" name="answer" required="required"></label></li>' );
-            }
-        function addQuestion() {
-            $('#Questions div:last').after( '<div><li><label><strong>Question </strong><input type="text" name="question" required="required"></label><br><label>Number of choiceable answers:<input type= "number" name="num_choiceable_answers" min="0" max="20"></label><ol id="Answers'+num_question+'"><li><label><strong>Answer </strong><input type="text" name="answer" required="required"></label></li><li><label><strong>Answer </strong><input type="text" name="answer" required="required"></label></li><button onclick="addAnswer('+num_question+'); return false;"><strong>+</stron></button><button onclick="removeAnswer('+num_question+'); return false;"><strong>-</stron></button></ol></li></div>' );
-            num_question++;
-            }
-        function removeAnswer(num) {
-            if($('#Answers'+num+'> li').length > 2)    $('#Answers'+num+' li:last').remove();
+            $('#Answers_'+num+' li:last').after( '<li><label><strong>Answer </strong><input type="text" name="answer" required="required"></label></li>' );
+            maxAnswers(num);
         }
-        function removeQuestion() {
+            
+        function addQuestion() {
+            $('#Questions div:last').after();
+            num_question++;
+        }
+            
+        function removeAnswer(num) {
+            if($('#Answers_'+num+'> li').length > 2)    $('#Answers_'+num+' li:last').remove();
+            maxAnswers(num);
+        }
+        
+        function removeQuestion(num) {
             if(num_question > 1){   
                     $('#Questions div:last').remove();
-                    num_question--;
                 }
         }
+        
+        function maxAnswers(num) { //atualiza o atributo 'max' no numero de respotas
+            var size = $('#Answers_'+num+'> li').length;
+            $('#min_answers_'+num).attr('max', size);
+            $('#max_answers_'+num).attr('max', size);
+        }
+                
+        function validateForm() {
+            var i;
+            for(i=1; i <= num_question; i++){
+                var size1 = $('#min_answers_'+i).val();
+                var size2 = $('#max_answers_'+i).val();
+                if(size1 > size2){
+                    //alert("O minimo é maior que o máximo.")
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
     </script>
 
     <head>
@@ -26,25 +51,27 @@
     </head>
     <body>
         <div>
-            <form action="newpoll.php" method="get">
+            <form action="newpoll.php" onsubmit="return validateForm()" method="get">
                 <ol id="Questions">
                     <div><li><label><strong>Question </strong>
-                                <input type="text" name="question" required="required"></label><br>
-                    <label>Number of choiceable answers:<input type= "number" name="num_choiceable_answers" min="0" max="20"></label>
-                    <ol id="Answers0">
+                        <input type="text" id="question_1" name="question_1" required="required"></label>
+                        <button onclick="removeQuestion(1); return false;"><strong>-</stron></button><br>
+                        <label>Number of min answers:<input value="1" type="number" id="min_answers_0" name="min_answers" min="1" max="2"></label>
+                        <label>Number of max answers:<input value="1" type="number" id="max_answers_0" name="max_answers" min="1" max="2"></label>
+                    <ol id="Answers_1">
                         <li><label><strong>Answer </strong>
-                            <input type="text" name="answer" required="required">
+                            <input type="text" name="answer_1_1" name="answer_1_1" required="required">
+                            <button onclick="removeAnswer(1); return false;"><strong>-</stron></button>
                             </label>
                         </li>
                         <li><label><strong>Answer </strong>
-                            <input type="text" name="answer" required="required">
+                            <input type="text" name="answer_1_2" name="answer_1_2" required="required">
+                            <button onclick="removeAnswer(2); return false;"><strong>-</stron></button>
                             </label>
                         </li>
-                        <button onclick="addAnswer(0); return false;"><strong>+</stron></button>
-                            <button onclick="removeAnswer(0); return false;"><strong>-</stron></button>
+                        <button onclick="addAnswer(); return false;"><strong>+</strong></button>
                     </ol></li></div>
-                    <button onclick="addQuestion(); return false;"><strong>+</stron></button>
-                        <button onclick="removeQuestion(); return false;"><strong>-</stron></button>
+                    <button onclick="addQuestion(); return false;"><strong>+</strong></button>
                 </ol>
                 <input type="submit" value="Submit">
             </form>
