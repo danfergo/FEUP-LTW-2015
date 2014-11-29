9da47394f302405bdbaafb6ac9d22fbb71ff86f3
Daniel Gomes <danfergo@gmail.com>
2014-11-29 16:39:38 +0000

    fixing some bugs with login/logout.

--- views/templates/page-sidebar-logged.php
+++ views/templates/page-sidebar-logged.php
@@ -1,13 +1,20 @@
+<div>
+    <a href="logout.php">   
+        <button type="button" class="btn btn-default">LOGOUT</button>
+    </a>
+</div>
+
+
 <nav>
     <ul>
         <li>
             <a href="createpoll.php"> Criar Votação </a>
         </li>
-         <li>
-            <a href="createpoll.php"> Minhas votações </a>
+        <li>
+            <a href="mypolls.php"> Minhas votações </a>
         </li>
-         <li>
-            <a href="createpoll.php"> Histórico </a>
+        <li>
+            <a href="history.php"> Histórico </a>
         </li>
     </ul>
 </nav>
