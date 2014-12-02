c37ba28e61824e1d1ec31372e448366bc076b505
Daniel Gomes <danfergo@gmail.com>
2014-12-02 17:31:18 +0000

    adding option to store temporary data in session.

--- actions/sessioning.php
+++ actions/sessioning.php
@@ -1,15 +1,25 @@
 <?php
+
 session_start();
 
-function session_begin($user){    
-    $_SESSION['qnaltw2015_userid'] = $user->getUserid();        
+function session_begin($user) {
+    $_SESSION['qnaltw2015_userid'] = $user->getUserid();
 }
 
-
-function session_exists(){
+function session_exists() {
     return isset($_SESSION['qnaltw2015_userid']);
 }
 
-function session_userid(){
+function session_userid() {
     return session_exists() ? $_SESSION['qnaltw2015_userid'] : false;
+}
+
+function session_setTempData($name, $data) {
+    $_SESSION["qnaltw2015_data"][$name] = $data;
+}
+
+function session_getTempData($name) {
+    $data = clone $_SESSION["qnaltw2015_data"][$name];
+    unset($_SESSION["qnaltw2015_data"][$name]);
+    return $data;
 }
