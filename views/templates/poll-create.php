f91bd8db5bebde0fad08a45d768ff7b453cc72a4
Daniel Gomes <danfergo@gmail.com>
2014-12-02 19:22:06 +0000

    updating template poll-create to delivery php data to javascript interface.

--- views/templates/poll-create.php
+++ views/templates/poll-create.php
@@ -1,39 +1,6 @@
-
 <div id="poll-create">
-    <form>
-
-        <ol class="questions">
-            <li class="question">
-                <input type="text"required="required">
-                <button class="remove-question">-</button>
-                <div>
-                    <label>
-                        Number of min answers:
-                        <input value="1" type="number"  min="1" max="2">
-                    </label>
-                    <label>
-                        Number of max answers:
-                        <input value="1" type="number"  min="1" max="2">
-                    </label>
-                </div>
-                <ol class="answers">
-                    <li class="answer">
-                        <input type="text"  required="required">
-                        <button class="remove-answer">-</button>
-                    </li>
-                    <li class="answer">
-                        <input type="text"  required="required">
-                        <button class="remove-answer">-</button>
-                    </li>
-                    <li>
-                        <input type="text" class="add-answer" value="Clique para adicionar opção.">
-                    </li>
-                </ol>
-            </li>
-            <li>
-                <input type="text" class="add-question" value="Clique para adicionar outra pergunta">
-            </li>
-        </ol>
-        <input type="submit" value="Submit">
-    </form>
+    <script type="text/javascript">
+        ERROR_MESSAGE = <?= json_encode($this->getErrorMsg()->getMessage()); ?>;
+        OLD_POLL_DATA = <?= json_encode($this->getOldPoll()); ?>;
+    </script>
 </div>
