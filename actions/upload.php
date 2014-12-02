62ea963b4da9a4edffbe7265f8ba237ab522bd08
Daniel Gomes <danfergo@gmail.com>
2014-12-02 16:07:44 +0000

    upload image file created, based on restivo's script.

---
+++ actions/upload.php
@@ -0,0 +1,39 @@
+<?php
+
+function valid_img($tmpName){
+    
+}
+
+
+
+function save_img($id, $tmpName) {
+
+    $originalFileName = "../img/polls/$id.jpg";
+    $smallFileName = "../img/thumb/$id.jpg";
+   // $mediumFileName = "images/thumbs_medium/$id.jpg";
+
+    move_uploaded_file($tmpName, $originalFileName);
+
+    $original = imagecreatefromjpeg($originalFileName);
+
+    $width = imagesx($original);
+    $height = imagesy($original);
+    $square = min($width, $height);
+
+    // Create small square thumbnail
+    $small = imagecreatetruecolor(200, 200);
+    imagecopyresized($small, $original, 0, 0, ($width > $square) ? ($width - $square) / 2 : 0, ($height > $square) ? ($height - $square) / 2 : 0, 200, 200, $square, $square);
+    imagejpeg($small, $smallFileName);
+
+    //$mediumwidth = $width;
+    //$mediumheight = $height;
+
+    //if ($mediumwidth > 400) {
+    //    $mediumwidth = 400;
+    //    $mediumheight = $mediumheight * ( $mediumwidth / $width );
+    //}
+
+    //$medium = imagecreatetruecolor($m     ediumwidth, $mediumheight);
+    //imagecopyresized($medium, $original, 0, 0, 0, 0, $mediumwidth, $mediumheight, $width, $height);
+    //imagejpeg($medium, $mediumFileName);
+}
