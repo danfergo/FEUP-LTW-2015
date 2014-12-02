4be409d472c495e1a4a27c8123b253f5c9120e55
Daniel Gomes <danfergo@gmail.com>
2014-12-02 19:22:48 +0000

    php PollView handles poll submit errors.

--- createpoll.php
+++ createpoll.php
@@ -2,15 +2,34 @@
 
 require_once ('views/zenpage.php');
 require_once ('actions/user.php');
+require_once ('actions/sessioning.php');
 
 
 $page = new ZenPage(__FILE__, user_who());
 
 class CreatePoll extends PrivateView {
 
+    private $oldPoll = null;
+    private $errorMsg = false;
+
     public function initializeForMember() {
         $this->getPage()->setPageTitle("Criar votação");
         $this->getPage()->addJavaScriptSrc("js/poll-create.js");
+        $this->setTemplate('poll-create');
+
+        if (session_tempDataIsset('POLL_CREATE_ERROR')) {
+            $this->oldPoll = session_getTempData('POLL_DATA');
+            $this->errorMsg = session_getTempData('POLL_CREATE_ERROR');
+            session_eraseTempData('POLL_CREATE_ERROR');
+        }
+    }
+
+    public function getOldPoll() {
+        return $this->oldPoll;
+    }
+
+    public function getErrorMsg() {
+        return $this->errorMsg;
     }
 
     public function initializeForPublic() {
