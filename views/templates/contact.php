f988b358b4786a836b9ebdda34f18cdcb07819c3
Nelson <paracontas987@hotmail.com>
2014-12-05 00:12:01 +0000

    Contact page com bootstrap

--- views/templates/contact.php
+++ views/templates/contact.php
@@ -1,43 +1,44 @@
 <style>
 
-    /*fieldset{
-        margin-top: 10px;
+    fieldset {
+        margin-bottom: 40px;
     }
 
-    fieldset:first-of-type{
-        margin-top: 0px;
-    }
-
-    #submitmessage{
-        //margin-top: 5px;
+    /*.btn-primary {
+        color: #fff;
+        background-color: #337ab7;
+        border-color: #2e6da4;
     }*/
 
 </style>
 
-<section>
-    <form>
 
+<div class="container-fluid">
+    <form>
+        <center><p><h1><strong>Contacto<strong></h1></p>
+            <p><h5>Caso nos deseje contactar pode enviar uma mensagem através do serviço disponibilizado.</h5></p>
+        </center>
         <fieldset>
             <legend>Informação Pessoal</legend>
-            <div class="container-fluid">
-                <label>Nome:
-                    <input type="text" name="username" required="required">
-                </label>
-            </div>
-
-            <div class="container-fluid">
-                <label>E-mail:
-                    <input type="mail" name="email" required="required">
-                </label>
+            <div class="row">
+                <div class="col-xs-12 col-md-3 form-group">
+                    <label>Nome:
+                        <input class="form-control" type="text" name="username" required="required">
+                    </label>
+                </div>
+                <div class="col-xs-12 col-md-3 form-group">
+                    <label>E-mail:
+                        <input class="form-control" type="mail" name="email" required="required">
+                    </label>
+                </div>
             </div>
         </fieldset>
 
         <fieldset>
             <legend>Informação a Enviar</legend>
-
-            <div class="container-fluid">
+            <div class="col-xs-12 col-md-3 form-group">
                 <label>Assunto:
-                    <select>
+                    <select class="form-control">
                         <option value="bug" selected>Reportar Erros</option>
                         <option value="poll">Reportar Questionáio</option>
                         <option value="sugestion">Sugestão</option>
@@ -49,14 +50,14 @@
 
             <br>
 
-            <div class="container-fluid">
+            <div class="form-group">
                 <label>Mensagem:
-                    <textarea name="message" rows="8" cols="50" required="required"></textarea>
+                    <textarea class="form-control" name="message" rows="8" cols="50" required="required"></textarea>
                 </label>
             </div>
 
         </fieldset>
-        <input id="submitmessage" type="submit" value="Enviar">
+        <input class="btn btn-primary" type="submit" value="Enviar">
     </form>
 
-</section>
+</div>
